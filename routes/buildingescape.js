79e0bac23dc07d684a886ddaf12ac66576ffdf9e
ice8scream <ice8scream@gmail.com>
2019-12-30 01:34:25 +0300

    another try

--- routes/buildingescape.js
+++ routes/buildingescape.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+    
+router.use(express.static(path.join(__dirname, '../', 'public/HTML5/')));
 
-router.use(express.static(path.join(__dirname, '../', 'public/games/buildingescape/')));
-
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.sendFile('index');
-    //res.render(path.join( 'games', 'buildingescape', 'index'));
-        //render('games/buildingescape/index', {title: 'game'});
+  res.sendFile('index');
+
 });
 
 module.exports = router;
